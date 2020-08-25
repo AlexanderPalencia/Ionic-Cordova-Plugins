760ca0568adb6b353d21ca29f970a33c7efaaba5
AlexanderPalencia <mapg23@outlook.com>
2020-08-25 00:07:34 -0600

    [feat] adding camara plugin

---
+++ src/app/pages/menu/menu.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.page.html',
+  styleUrls: ['./menu.page.scss'],
+})
+export class MenuPage implements OnInit {
+
+  myImg = null;
+  constructor(private camera:Camera) { }
+
+  ngOnInit() {
+  }
+
+  capture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+     this.myImg = base64Image;
+    }, (err) => {
+     // Handle error
+    });
+  }
+
+}
